04855e673a698790eece7290602254658783ca00
AyushMali-17 <ayushmali1708@gmail.com>
2024-08-10 17:44:45 +0000

    just started -1

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// JavaScript for handling the modal functionality
+
+// Get the modal
+var modal = document.getElementById("imageModal");
+
+// Get the image and insert it inside the modal
+var modalImg = document.getElementById("modalImage");
+
+var images = document.getElementsByClassName("gallery-image");
+
+for (var i = 0; i < images.length; i++) {
+    images[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
