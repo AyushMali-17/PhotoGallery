905388af318ee0baa7b2677762781c0fd2e6db2a
AyushMali-17 <ayushmali1708@gmail.com>
2024-08-10 18:00:53 +0000

    made the basic version-4

--- scripts.js
+++ scripts.js
@@ -1,15 +1,13 @@
-// JavaScript for handling the modal functionality
-
 // Get the modal and related elements
 var modal = document.getElementById("imageModal");
 var modalImg = document.getElementById("modalImage");
 var captionText = document.getElementById("caption");
-
-// Get the images and initialize current image index
 var images = document.getElementsByClassName("gallery-image");
 var currentIndex;
+var slideshowInterval;
+var isDarkMode = false;
 
-// Display image in modal and set caption
+// Function to open modal and display the clicked image
 function openModal(index) {
     modal.style.display = "block";
     modalImg.src = images[index].src;
@@ -17,64 +15,103 @@ function openModal(index) {
     currentIndex = index;
 }
 
-// Close the modal
+// Function to close the modal
 function closeModal() {
     modal.style.display = "none";
+    clearInterval(slideshowInterval);
 }
 
-// Show the previous image
+// Function to show the previous image
 function showPrev() {
     currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
     openModal(currentIndex);
 }
 
-// Show the next image
+// Function to show the next image
 function showNext() {
     currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
     openModal(currentIndex);
 }
 
-// Attach click events to images
-for (var i = 0; i < images.length; i++) {
-    images[i].onclick = function() {
-        var index = Array.prototype.indexOf.call(images, this);
-        openModal(index);
+// Function to start slideshow
+function startSlideshow() {
+    slideshowInterval = setInterval(showNext, 3000);
+    document.getElementById("slideshowToggle").innerText = "Stop Slideshow";
+    document.getElementById("slideshowToggle").onclick = stopSlideshow;
+}
+
+// Function to stop slideshow
+function stopSlideshow() {
+    clearInterval(slideshowInterval);
+    document.getElementById("slideshowToggle").innerText = "Start Slideshow";
+    document.getElementById("slideshowToggle").onclick = startSlideshow;
+}
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    isDarkMode = !isDarkMode;
+    document.body.classList.toggle("dark-mode", isDarkMode);
+    document.getElementById("darkModeToggle").innerText = isDarkMode ? "Light Mode" : "Dark Mode";
+}
+
+// Image upload and gallery update
+var imageUpload = document.getElementById("imageUpload");
+imageUpload.addEventListener("change", function(event) {
+    var files = event.target.files;
+    var galleryGrid = document.getElementById("galleryGrid");
+
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            var newImage = document.createElement("img");
+            newImage.src = e.target.result;
+            newImage.alt = "User Uploaded Image";
+            newImage.classList.add("gallery-image");
+            galleryGrid.appendChild(newImage);
+
+            // Add click event for the new image
+            newImage.addEventListener("click", function() {
+                openModal(Array.from(images).indexOf(newImage));
+            });
+        };
+
+        reader.readAsDataURL(file);
     }
-}
+});
 
-// Attach event listeners for modal controls
-document.getElementsByClassName("close")[0].onclick = function() { 
-    closeModal();
-}
+// Add event listeners to gallery images
+Array.from(images).forEach((image, index) => {
+    image.addEventListener("click", function() {
+        openModal(index);
+    });
+});
 
-document.getElementsByClassName("prev")[0].onclick = function() { 
-    showPrev();
-}
+// Close the modal when the user clicks on <span> (x)
+document.getElementsByClassName("close")[0].onclick = closeModal;
 
-document.getElementsByClassName("next")[0].onclick = function() { 
-    showNext();
-}
+// Navigate with arrows
+document.getElementsByClassName("prev")[0].onclick = showPrev;
+document.getElementsByClassName("next")[0].onclick = showNext;
+
+// Toggle slideshow
+document.getElementById("slideshowToggle").onclick = startSlideshow;
+
+// Toggle dark mode
+document.getElementById("darkModeToggle").onclick = toggleDarkMode;
 
 // Keyboard navigation
-document.onkeydown = function(event) {
-    if (modal.style.display === "block") {
-        switch (event.keyCode) {
-            case 37: // Left arrow key
-                showPrev();
-                break;
-            case 39: // Right arrow key
-                showNext();
-                break;
-            case 27: // Escape key
-                closeModal();
-                break;
-        }
+document.onkeydown = function(e) {
+    switch (e.key) {
+        case "ArrowLeft":
+            showPrev();
+            break;
+        case "ArrowRight":
+            showNext();
+            break;
+        case "Escape":
+            closeModal();
+            break;
     }
-}
-
-// Close modal on clicking outside of the image
-window.onclick = function(event) {
-    if (event.target == modal) {
-        closeModal();
-    }
-}
+};
