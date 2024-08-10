c2685222a6be03e827b5b8841590c2c39a4d3891
AyushMali-17 <ayushmali1708@gmail.com>
2024-08-10 17:49:06 +0000

    adding more function  -2

--- scripts.js
+++ scripts.js
@@ -22,3 +22,10 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
 }
+
+// Close modal on clicking outside of the image
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
